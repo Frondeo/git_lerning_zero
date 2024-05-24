8b6f0f679ddd0eb3795ff2f30b88ebe56b2f936d
andrey <me@messus.ru>
2024-05-24 20:56:15 +0700

    add def divided

--- first/nums.py
+++ first/nums.py
@@ -9,9 +9,15 @@ def minus(a,b):
 def multi(a,b):
     return a*b
 
+
+def divided(a,b):
+    return a/b
+
 if __name__ == '__main__':
     print(plus(2,2))
     print(minus(4,2))
     print(multi(3,3))
+    print(divided(4,2))
+
 
 
